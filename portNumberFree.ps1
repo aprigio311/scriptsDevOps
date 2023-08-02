67cf1440a502f589972a34bf29601260a4ad6934
Aprigio4pr <65431209+aprigio311@users.noreply.github.com>
2023-08-02 17:58:04 -0300

    upload
    verificação de portas

---
+++ portNumberFree.ps1
@@ -0,0 +1,15 @@
+﻿#Script verifica se uma porta esta em uso ou nao, voce tem que digitar a porta que quer verificar!
+
+$Port = Read-Host "Digite a porta que gostaria de verificar"
+
+$cmd1 = netstat -an | findstr "$Port"
+
+if ($cmd1.Length -gt 0)
+    {
+    echo "A porta $Port esta ocupada"
+    }
+
+else
+    {
+    echo "A porta $Port esta livre"
+    }
